fb6c9fba01b7ae15ba0dbbbaf5767795e5dc56d4
Kyle Fossman <kjfossman@gmail.com>
2021-01-16 17:10:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 
 // Add your functions and code here
-destructivelyAppendKitten = function(array, element){
+let destructivelyAppendKitten = function(array, element){
   array.push(element)
   return array
 }
